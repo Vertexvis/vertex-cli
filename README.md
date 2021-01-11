eff2d8dd5169ee778c68d106807e2a234a88366f
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2021-01-11 11:25:16 -0700

    [API-1517]: Add option to create Web SDK Viewer HTML (#32)

--- README.md
+++ README.md
@@ -26,7 +26,7 @@ $ npm install -g @vertexvis/vertex-cli
 $ vertex COMMAND
 running command...
 $ vertex (-v|--version|version)
-@vertexvis/vertex-cli/0.3.4 darwin-x64 node-v14.15.3
+@vertexvis/vertex-cli/0.3.5 darwin-x64 node-v14.15.4
 $ vertex --help [COMMAND]
 USAGE
   $ vertex COMMAND
@@ -72,7 +72,7 @@ EXAMPLE
   Wrote 5 pvs item(s) from 'path/to/file' to 'items.json'.
 ```
 
-_See code: [src/commands/create-items.ts](https://github.com/Vertexvis/vertex-cli/blob/v0.3.4/src/commands/create-items.ts)_
+_See code: [src/commands/create-items.ts](https://github.com/Vertexvis/vertex-cli/blob/v0.3.5/src/commands/create-items.ts)_
 
 ## `vertex create-parts [PATH]`
 
@@ -95,7 +95,7 @@ EXAMPLE
   Uploading file(s) and creating part(s)... done
 ```
 
-_See code: [src/commands/create-parts.ts](https://github.com/Vertexvis/vertex-cli/blob/v0.3.4/src/commands/create-parts.ts)_
+_See code: [src/commands/create-parts.ts](https://github.com/Vertexvis/vertex-cli/blob/v0.3.5/src/commands/create-parts.ts)_
 
 ## `vertex create-scene [PATH]`
 
@@ -117,7 +117,7 @@ EXAMPLE
   Created scene f79d4760-0b71-44e4-ad0b-22743fdd4ca3.
 ```
 
-_See code: [src/commands/create-scene.ts](https://github.com/Vertexvis/vertex-cli/blob/v0.3.4/src/commands/create-scene.ts)_
+_See code: [src/commands/create-scene.ts](https://github.com/Vertexvis/vertex-cli/blob/v0.3.5/src/commands/create-scene.ts)_
 
 ## `vertex create-stream-key [ID]`
 
@@ -138,7 +138,7 @@ EXAMPLE
   Created stream-key 'hBXAoQdnsHVhgDZkxeLEPQVxPJ600QwDMdgq' expiring in 600 seconds.
 ```
 
-_See code: [src/commands/create-stream-key.ts](https://github.com/Vertexvis/vertex-cli/blob/v0.3.4/src/commands/create-stream-key.ts)_
+_See code: [src/commands/create-stream-key.ts](https://github.com/Vertexvis/vertex-cli/blob/v0.3.5/src/commands/create-stream-key.ts)_
 
 ## `vertex delete [ID]`
 
@@ -160,7 +160,7 @@ EXAMPLE
   Delete scene(s) f79d4760-0b71-44e4-ad0b-22743fdd4ca3.
 ```
 
-_See code: [src/commands/delete.ts](https://github.com/Vertexvis/vertex-cli/blob/v0.3.4/src/commands/delete.ts)_
+_See code: [src/commands/delete.ts](https://github.com/Vertexvis/vertex-cli/blob/v0.3.5/src/commands/delete.ts)_
 
 ## `vertex help [COMMAND]`
 
@@ -181,7 +181,7 @@ _See code: [@oclif/plugin-help](https://github.com/oclif/plugin-help/blob/v3.2.1
 
 ## `vertex render-image [ID]`
 
-Render an image of either a scene or scene-view.
+Render an image or create Web SDK Viewer HTML for either a scene or scene-view.
 
 ```
 USAGE
@@ -195,11 +195,12 @@ OPTIONS
   -r, --resource=scene|scene-view  [default: scene] Resource type of ID provided.
   -v, --verbose
   -w, --width=width                [default: 100] Image width.
+  --viewer                         Create Web SDK Viewer HTML instead of jpg image.
 
 EXAMPLE
   $ vertex render-image f79d4760-0b71-44e4-ad0b-22743fdd4ca3
   Image written to 'f79d4760-0b71-44e4-ad0b-22743fdd4ca3.jpg'.
 ```
 
-_See code: [src/commands/render-image.ts](https://github.com/Vertexvis/vertex-cli/blob/v0.3.4/src/commands/render-image.ts)_
+_See code: [src/commands/render-image.ts](https://github.com/Vertexvis/vertex-cli/blob/v0.3.5/src/commands/render-image.ts)_
 <!-- commandsstop -->
--- src/commands/render-image.ts
+++ src/commands/render-image.ts
@@ -5,11 +5,11 @@ import {
   renderSceneView,
   VertexClient,
 } from '@vertexvis/vertex-api-client';
-import { createWriteStream } from 'fs';
+import { createWriteStream, writeFileSync } from 'fs';
 import BaseCommand from '../base';
 
 export default class RenderImage extends BaseCommand {
-  public static description = `Render an image of either a scene or scene-view.`;
+  public static description = `Render an image or create Web SDK Viewer HTML for either a scene or scene-view.`;
 
   public static examples = [
     `$ vertex render-image f79d4760-0b71-44e4-ad0b-22743fdd4ca3
@@ -21,6 +21,10 @@ Image written to 'f79d4760-0b71-44e4-ad0b-22743fdd4ca3.jpg'.
 
   public static flags = {
     ...BaseCommand.flags,
+    viewer: flags.boolean({
+      description: 'Create Web SDK Viewer HTML instead of jpg image.',
+      default: false,
+    }),
     height: flags.integer({
       char: 'h',
       description: 'Image height.',
@@ -49,22 +53,41 @@ Image written to 'f79d4760-0b71-44e4-ad0b-22743fdd4ca3.jpg'.
     if (flags.width < 1) this.error(`Invalid width ${flags.width}.`);
 
     try {
-      const renderArgs = {
-        client: await VertexClient.build({ basePath: flags.basePath }),
-        renderReq: { id: args.id, height: flags.height, width: flags.width },
-      };
-      const renderRes = await (flags.resource === 'scene'
-        ? renderScene<NodeJS.ReadableStream>(renderArgs)
-        : renderSceneView<NodeJS.ReadableStream>(renderArgs));
-      if (parseInt(renderRes.headers['content-length'], 10) < 140) {
-        this.error(`Received empty image for ${flags.resource} ${args.id}.`);
+      const client = await VertexClient.build({ basePath: flags.basePath });
+      if (flags.viewer) {
+        const streamKeyRes = await client.streamKeys.createSceneStreamKey({
+          id: args.id,
+          createStreamKeyRequest: {
+            data: {
+              attributes: {},
+              type: 'stream-key',
+            },
+          },
+        });
+        const output = flags.output || `${args.id}.html`;
+        const key = streamKeyRes.data.data.attributes.key;
+        if (!key) this.error('Error creating stream-key');
+        writeFileSync(output, generateHtml(key));
+
+        this.log(`Viewer HTML written to '${output}'.`);
+      } else {
+        const renderArgs = {
+          client,
+          renderReq: { id: args.id, height: flags.height, width: flags.width },
+        };
+        const renderRes = await (flags.resource === 'scene'
+          ? renderScene<NodeJS.ReadableStream>(renderArgs)
+          : renderSceneView<NodeJS.ReadableStream>(renderArgs));
+        if (parseInt(renderRes.headers['content-length'], 10) < 140) {
+          this.error(`Received empty image for ${flags.resource} ${args.id}.`);
+        }
+
+        const output = flags.output || `${args.id}.jpg`;
+        renderRes.data.pipe(createWriteStream(output));
+        await createFile(renderRes.data, output);
+
+        this.log(`Image written to '${output}'.`);
       }
-
-      const output = flags.output || `${args.id}.jpg`;
-      renderRes.data.pipe(createWriteStream(output));
-      await createFile(renderRes.data, output);
-
-      this.log(`Image written to '${output}'.`);
     } catch (error) {
       logError(error, this.error);
     }
@@ -80,4 +103,64 @@ async function createFile(
     stream.pipe(ws);
     ws.on('finish', resolve);
   });
+}
+
+function generateHtml(streamKey: string): string {
+  return `<html>
+  <head>
+    <meta charset="utf-8" />
+    <link
+      rel="stylesheet"
+      href="https://unpkg.com/@vertexvis/viewer@0.9.x/dist/viewer/viewer.css"
+    />
+    <script
+      type="module"
+      src="https://unpkg.com/@vertexvis/viewer@0.9.x/dist/viewer/viewer.esm.js"
+    ></script>
+    <script
+      nomodule
+      src="https://unpkg.com/@vertexvis/viewer@0.9.x/dist/viewer.js"
+    ></script>
+  </head>
+  <body>
+    <vertex-viewer id="viewer" class="viewer" client-id="08F675C4AACE8C0214362DB5EFD4FACAFA556D463ECA00877CB225157EF58BFA" style="width: auto; height: auto">
+    </vertex-viewer>
+
+    <script type="module">
+      import { ColorMaterial } from 'https://unpkg.com/@vertexvis/viewer@0.9.x/dist/esm/index.mjs';
+
+      document.addEventListener('DOMContentLoaded', () => {
+        main();
+      });
+
+      async function main() {
+        const viewer = document.querySelector('vertex-viewer');
+        await viewer.load('urn:vertexvis:stream-key:${streamKey}');
+
+        viewer.addEventListener('tap', async (event) => {
+          const { position } = event.detail;
+          const scene = await viewer.scene();
+          const raycaster = await scene.raycaster();
+
+          const result = await raycaster.hitItems(position);
+
+          if (result.hits && result.hits.length == 0) {
+            await scene
+              .items((op) => op.where((q) => q.all()).clearMaterialOverrides())
+              .execute();
+          } else {
+            await scene
+              .items((op) => [
+                op.where((q) => q.all()).clearMaterialOverrides(),
+                op
+                  .where((q) => q.withItemId(result.hits[0].itemId.hex))
+                  .materialOverride(ColorMaterial.fromHex('#ff0000')),
+              ])
+              .execute();
+          }
+        });
+      }
+    </script>
+  </body>
+</html>`;
 }
