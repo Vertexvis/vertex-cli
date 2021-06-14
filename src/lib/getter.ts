4a3c5592fdde51a4eaee9a1ca36e953cfb9f768d
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2021-06-14 13:52:17 -0600

    Add scene-view-state CRUD operations

--- src/lib/getter.ts
+++ src/lib/getter.ts
@@ -6,6 +6,7 @@ import {
   PartData,
   prettyJson,
   SceneData,
+  SceneViewStateData,
 } from '@vertexvis/api-client-node';
 import cli from 'cli-ux';
 
@@ -16,7 +17,7 @@ interface Paged<T> {
 
 interface Getter<T> {
   readonly getOne: (id: string) => Promise<T>;
-  readonly getAll: (cursor?: string) => Promise<Paged<T>>;
+  readonly getAll: (cursor?: string, id?: string) => Promise<Paged<T>>;
   readonly displayOne: (res: Paged<T>) => void;
   readonly displayAll: (res: Paged<T>, extended: boolean) => void;
 }
@@ -35,7 +36,7 @@ export async function getterFn<T>({
   id?: string;
 }): Promise<void> {
   if (all) {
-    const res = await getter.getAll(cursor);
+    const res = await getter.getAll(cursor, id);
     getter.displayAll(res, extended);
     if (res.cursor) console.log(res.cursor);
   } else if (id) {
@@ -119,6 +120,41 @@ export function sceneGetter({ client }: BaseReq): Getter<SceneData> {
           name: { minWidth: 12 },
           suppliedId: { extended: true, header: 'SuppliedId' },
           state: { extended: true },
+          created: { extended: true },
+        },
+        { extended }
+      ),
+  };
+}
+
+export function sceneViewStateGetter({
+  client,
+}: BaseReq): Getter<SceneViewStateData> {
+  return {
+    getOne: async (id: string): Promise<SceneViewStateData> => {
+      return (await client.sceneViewStates.getSceneViewState({ id })).data.data;
+    },
+    getAll: async (
+      cursor?: string,
+      id?: string
+    ): Promise<Paged<SceneViewStateData>> => {
+      const res = await getPage(() =>
+        client.sceneViewStates.getSceneViewStates({
+          id: id ?? '',
+          pageCursor: cursor,
+          pageSize: 25,
+        })
+      );
+      return { items: res.page.data, cursor: res.cursor };
+    },
+    displayOne: (res: Paged<SceneViewStateData>): void =>
+      console.log(prettyJson(head(res.items))),
+    displayAll: (res: Paged<SceneViewStateData>, extended: boolean): void =>
+      cli.table(
+        res.items.map((f) => ({ id: f.id, ...f.attributes })),
+        {
+          id: { minWidth: 36 },
+          name: { minWidth: 12 },
           created: { extended: true },
         },
         { extended }
