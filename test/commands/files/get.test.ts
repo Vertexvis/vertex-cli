5f43e858da52f42945c66818bac2c55e2cb88ae3
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2021-09-30 16:48:49 -0600

    Add tests

---
+++ test/commands/files/get.test.ts
@@ -0,0 +1,39 @@
+import { IConfig } from '@oclif/config';
+import { expect, test } from '@oclif/test';
+import { VertexClient } from '@vertexvis/api-client-node';
+import sinon from 'sinon';
+
+import Get from '../../../src/commands/files/get';
+import * as vc from '../../../src/lib/client';
+import * as g from '../../../src/lib/getter';
+
+const id = 'my-id';
+
+describe('files:get', () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  test
+    .command(['files:get'])
+    .catch((error) => {
+      expect(error.message).to.contain('Missing 1 required arg:');
+      expect(error.message).to.contain('id');
+    })
+    .it('requires id');
+
+  test
+    .stdout()
+    .do(async () => {
+      sinon
+        .stub(vc, 'vertexClient')
+        .resolves(sinon.stub() as unknown as VertexClient);
+      sinon.stub(g, 'getterFn').resolves(sinon.stub() as unknown as void);
+      sinon.stub(g, 'fileGetter').resolves(sinon.stub());
+
+      await new Get([id], {} as IConfig).run();
+    })
+    .it('works', (ctx) => {
+      expect(ctx.stdout).to.contain('');
+    });
+});
